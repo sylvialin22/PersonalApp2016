5c34474e6bc4423ced185b891f85ac770079fc9f
sylvialin22 <sblin@brandeis.edu>
2016-06-21 10:25:16 -0400

    used a template state

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -11,6 +11,27 @@ Template.home.events({
     $(".js-name").val("");
     $(".js-items").val("");
     console.log("SUCCESS");
-    
+
   }
+})
+
+Template.home.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    color: "bg-info",
+    counter: 0,
+  });
+  console.log("creating the template");
+  console.dir(this.state);
+});
+
+Template.home.helpers({
+  theColor: function(){
+    const instance = Template.instance();
+    return instance.state.get("color");
+  },
+  theCounter: function(){
+    const instance = Template.instance();
+    return instance.state.get("counter");
+  },
 })
