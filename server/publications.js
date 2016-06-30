95b8145f186be8ab8aab6b575ba48a57a235a36f
sylvialin22 <sblin@brandeis.edu>
2016-06-30 01:59:53 -0400

    publications and subscriptions

--- server/publications.js
+++ server/publications.js
@@ -4,17 +4,14 @@ Meteor.publish("theComments",
 Meteor.publish("theLists",
   function(){return Lists.find();})
 
-Meteor.publish("theListsTravel",
-  function(p){return Lists.find({items:p});})
+Meteor.publish("userData", function () {
+  if (this.userId) {  //only if you log in you can see it
+	  //return Meteor.users.find({}); // everyone sees everything
+                             //{fields: {'profile': 1, 'things': 1}});
 
-  Meteor.publish("userData", function () {
-    if (this.userId) {  //only if you log in you can see it
-  	  //return Meteor.users.find({}); // everyone sees everything
-                               //{fields: {'profile': 1, 'things': 1}});
-
-    return Meteor.users.find({_id: this.userId},
-            {fields:{profile:1, "services.google.email":1}});
-    } else {
-      this.ready();
-    }
-  });
+  return Meteor.users.find({_id: this.userId},
+          {fields:{profile:1, "services.google.email":1}});
+  } else {
+    this.ready();
+  }
+});
